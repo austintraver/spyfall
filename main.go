35f827b5d5aef701075a73e43a76da2202034198
Austin Traver <austintraver@gmail.com>
2022-01-06 20:45:32 -0800

    Implement server-side events

--- main.go
+++ main.go
@@ -12,21 +12,36 @@ import (
 	"gopkg.in/yaml.v3"
 )
 
+var (
+	port = 1337
+)
+
 // Player represents the structure of a Spyfall player.
 type Player struct {
-	Name     string
-	Role     string
-	Location string
+	Name string
+	Role string
 }
 
+// Location represents the current setting of the current game of Spyfall.
 type Location struct {
 	Name string
 	Role []string
 }
 
-type Hourglass struct {
+// Clock represents the current status of the in-game timer. When there is no
+// game in process, "start" will be set to the zero-value of time.Time. For now,
+// the value of duration is set to the default value of 5 minutes.
+type Clock struct {
 	start    time.Time
 	duration time.Duration
+}
+
+// Lobby represents a Spyfall lobby, in which there is a host, a list of
+// players, and potentially, an active game taking place.
+type Lobby struct {
+	location Location
+	clock    Clock
+	player   []Player
 }
 
 func main() {
@@ -40,19 +55,12 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	// Randomize the order of the locations in the list.
-	rand.Shuffle(len(locations), func(i, j int) {
-		locations[i], locations[j] = locations[j], locations[i]
-	})
-
-	// Choose a location for this game, the first index of the now-shuffled elements.
-	location := locations[0]
-
-	roles := location.Role
+	// Choose a location for this game from the list of available locations.
+	l := locations[rand.Intn(len(locations))]
 
 	// TODO: support custom duration of time specified by the admin of the lobby
 
-	timers := map[int]Hourglass{
+	timers := map[int]Clock{
 		1: {
 			start:    time.Now(),
 			duration: time.Minute * 5,
@@ -77,33 +85,86 @@ func main() {
 		}
 	})
 
-	http.HandleFunc("/play", func(w http.ResponseWriter, r *http.Request) {
-		t, e := template.ParseFiles("tmpl/example.html")
-		// Check for errors.
+	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Connection", "keep-alive")
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(
+				w,
+				"Server does not support Flusher!",
+				http.StatusInternalServerError,
+			)
+			return
+		}
+		for {
+			time.Sleep(1 * time.Second)
+			_, err = fmt.Fprintf(w, "data: logging %v...", time.Now())
+			if err != nil {
+				// TODO: Handle the "broken pipe" error that is thrown when a
+				//  user prematurely leaves the lobby (exits the page).
+				panic(err)
+			}
+			_, err = fmt.Fprintf(w, "\n\n")
+			if err != nil {
+				panic(err)
+			}
+			flusher.Flush()
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// Parse the template file.
+		t, e := template.ParseFiles("tmpl/index.html")
 		if e != nil {
 			panic(err)
 		}
-		// Randomize the order of the roles in the location.
-		rand.Shuffle(len(roles), func(i, j int) {
-			roles[i], roles[j] = roles[j], roles[i]
-		})
-		role := roles[0]
+		// Select a random role among the roles available in this location.
+		role := l.Role[rand.Intn(len(l.Role))]
+
+		// Extract the name from the HTTP request.
+		name := r.FormValue("name")
 
 		// Create a player with the name provided in the HTTP request,
 		// and assign the player a random role from the now-shuffled list.
 		p := Player{
-			Name:     r.FormValue("name"),
-			Role:     role,
-			Location: location.Name,
+			Name: name,
+			Role: role,
 		}
+
+		// Output the templated HTML file in response to this HTTP request.
 		e = t.Execute(w, p)
-		// e = t.Execute(os.Stdout, p)
 		if e != nil {
 			panic(err)
 		}
 	})
-	err = http.ListenAndServe("[::1]:1337", nil)
+
+	// Format the hostname and port number to use for the HTTP server.
+	authority := fmt.Sprintf(":%d", port)
+
+	// Listen for incoming HTTP requests.
+	err = http.ListenAndServe(authority, nil)
 	if err != nil {
 		panic(err)
+	}
+}
+
+func init() {
+	// Attempt to read a custom port number, if one is provided in the form
+	// of an environment variable.
+	val := os.Getenv("SPYFALL_PORT")
+	if val != "" {
+		// Only alter the value of the port number if it is a valid integer.
+		num, err := strconv.Atoi(val)
+		if err == nil {
+			port = num
+		} else {
+			fmt.Fprintf(
+				os.Stderr,
+				"Error parsing port from environment variable 'SPYFALL_PORT'... Listening on port %d instead\n",
+				port,
+			)
+		}
 	}
 }
