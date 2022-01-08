a8a693bc0199bff6514d0ebcc8815895cde83364
Austin Traver <austintraver@gmail.com>
2022-01-08 03:53:15 -0800

    Restructure project scaffolding
    This commit modifies the directory scaffolding, but makes minimal
    alterations to the underlying codebase. These changes to the
    structure of the codebase are made to better-align this project
    with common software design patterns employed in full-stack web
    applications.

--- main.go
+++ main.go
@@ -1,8 +1,11 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"html/template"
+	"io"
+	"io/fs"
 	"math/rand"
 	"net/http"
 	"os"
@@ -45,6 +48,53 @@ type Lobby struct {
 }
 
 func main() {
+	// Register handlers for requests to different paths.
+	http.HandleFunc("/time", handleTime)
+	http.HandleFunc("/events", handleEvent)
+	http.HandleFunc("/", handleDefault)
+	// http.Handle("/public", http.FileServer(http.Dir("./public")))
+
+	// Format the server's hostname and port number.
+	authority := fmt.Sprintf(":%d", port)
+
+	// Listen for incoming HTTP requests.
+	fmt.Printf("Mixing it up at http://localhost:%d\n", port)
+	err := http.ListenAndServe(authority, nil)
+	if err != nil {
+		panic(err)
+	}
+}
+
+func handleDefault(w http.ResponseWriter, r *http.Request) {
+	// Handle requests to the homepage "index.html", by responding with
+	// a formatted version of the templated "index.html" file.
+	if r.URL.Path == "/" || r.URL.Path == "/index.html" {
+		serveIndex(w, r)
+		return
+	}
+	// Create a filesystem rooted at the "/public" subdirectory.
+	fsys := os.DirFS("./public")
+	file, err := fsys.Open(r.URL.Path[1:])
+	// Notify the user if the requested file is not found.
+	if errors.Is(err, fs.ErrNotExist) {
+		http.NotFound(w, r)
+	} else if err != nil {
+		// Panic if any other error is encountered.
+		panic(err)
+	} else {
+		// If there is no error, the file was found, so output write it as
+		// the response to this request.
+		_, err = io.Copy(w, file)
+		// Panic if we were unable to do so.
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+// Responds to a request by providing a formatted version of the templated
+// "index.html" file.
+func serveIndex(w http.ResponseWriter, r *http.Request) {
 	// Read in the list of locations and roles from a file.
 	data, err := os.ReadFile("data/location.yaml")
 	if err != nil {
@@ -55,11 +105,70 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
+
 	// Choose a location for this game from the list of available locations.
 	l := locations[rand.Intn(len(locations))]
+	// Parse the template file.
+	t, e := template.ParseFiles("tmpl/index.html")
+	if e != nil {
+		panic(err)
+	}
+	// Select a random role among the roles available in this location.
+	role := l.Role[rand.Intn(len(l.Role))]
 
-	// TODO: support custom duration of time specified by the admin of the lobby
+	// Extract the name from the HTTP request.
+	name := r.FormValue("name")
 
+	// Create a player with the name provided in the HTTP request,
+	// and assign the player a random role from the now-shuffled list.
+	p := Player{
+		Name: name,
+		Role: role,
+	}
+
+	// Output the templated HTML file in response to this HTTP request.
+	e = t.Execute(w, p)
+	if e != nil {
+		panic(err)
+	}
+	return
+}
+
+func handleEvent(w http.ResponseWriter, r *http.Request) {
+	// Set the headers necessary to output a server-side event.
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-Type", "text/event-stream")
+	w.Header().Set("Connection", "keep-alive")
+	flusher, ok := w.(http.Flusher)
+	if !ok {
+		http.Error(
+			w,
+			"Server does not support Flusher!",
+			http.StatusInternalServerError,
+		)
+		return
+	}
+	// Output a "data" segment every second.
+	// TODO: provide more useful server-side events, such as
+	//  user renames, users joining the lobby, and users leaving the lobby.
+	for {
+		time.Sleep(1 * time.Second)
+		_, err := fmt.Fprintf(w, "data: logging %v...", time.Now())
+		if err != nil {
+			// TODO: Handle the "broken pipe" error that is thrown when a
+			//  user prematurely leaves the lobby (exits the page).
+			panic(err)
+		}
+		_, err = fmt.Fprintf(w, "\n\n")
+		if err != nil {
+			panic(err)
+		}
+		flusher.Flush()
+	}
+}
+
+// TODO: Utilize this request on the front-end.
+func handleTime(w http.ResponseWriter, r *http.Request) {
 	timers := map[int]Clock{
 		1: {
 			start:    time.Now(),
@@ -67,86 +176,20 @@ func main() {
 		},
 	}
 
-	http.HandleFunc("/time", func(w http.ResponseWriter, r *http.Request) {
-		id, e := strconv.Atoi(r.FormValue("id"))
-		if e != nil {
-			panic(e)
-		}
-		t, ok := timers[id]
-		if !ok {
-			w.WriteHeader(http.StatusBadRequest)
-			_, _ = fmt.Fprintf(w, "Invalid timer id %d", id)
-			return
-		}
-		remaining := time.Until(t.start.Add(t.duration))
-		_, e = fmt.Fprintf(w, "%v\n", remaining)
-		if e != nil {
-			panic(e)
-		}
-	})
-
-	http.HandleFunc("/events", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Cache-Control", "no-cache")
-		w.Header().Set("Content-Type", "text/event-stream")
-		w.Header().Set("Connection", "keep-alive")
-		flusher, ok := w.(http.Flusher)
-		if !ok {
-			http.Error(
-				w,
-				"Server does not support Flusher!",
-				http.StatusInternalServerError,
-			)
-			return
-		}
-		for {
-			time.Sleep(1 * time.Second)
-			_, err = fmt.Fprintf(w, "data: logging %v...", time.Now())
-			if err != nil {
-				// TODO: Handle the "broken pipe" error that is thrown when a
-				//  user prematurely leaves the lobby (exits the page).
-				panic(err)
-			}
-			_, err = fmt.Fprintf(w, "\n\n")
-			if err != nil {
-				panic(err)
-			}
-			flusher.Flush()
-		}
-	})
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Parse the template file.
-		t, e := template.ParseFiles("tmpl/index.html")
-		if e != nil {
-			panic(err)
-		}
-		// Select a random role among the roles available in this location.
-		role := l.Role[rand.Intn(len(l.Role))]
-
-		// Extract the name from the HTTP request.
-		name := r.FormValue("name")
-
-		// Create a player with the name provided in the HTTP request,
-		// and assign the player a random role from the now-shuffled list.
-		p := Player{
-			Name: name,
-			Role: role,
-		}
-
-		// Output the templated HTML file in response to this HTTP request.
-		e = t.Execute(w, p)
-		if e != nil {
-			panic(err)
-		}
-	})
-
-	// Format the hostname and port number to use for the HTTP server.
-	authority := fmt.Sprintf(":%d", port)
-
-	// Listen for incoming HTTP requests.
-	err = http.ListenAndServe(authority, nil)
-	if err != nil {
-		panic(err)
+	id, e := strconv.Atoi(r.FormValue("id"))
+	if e != nil {
+		panic(e)
+	}
+	t, ok := timers[id]
+	if !ok {
+		w.WriteHeader(http.StatusBadRequest)
+		_, _ = fmt.Fprintf(w, "Invalid timer id %d", id)
+		return
+	}
+	remaining := time.Until(t.start.Add(t.duration))
+	_, e = fmt.Fprintf(w, "%v\n", remaining)
+	if e != nil {
+		panic(e)
 	}
 }
 
